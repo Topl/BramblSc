5879870dccb4796d7508a697dc9ba37a51d616e0
mgrand <m.grand@topl.me>
2022-12-19 14:29:50 -0500

    Added comments

--- crypto/src/main/scala/co/topl/crypto/accumulators/accumulators.scala
+++ crypto/src/main/scala/co/topl/crypto/accumulators/accumulators.scala
@@ -14,21 +14,6 @@ package object accumulators {
   @newtype
   case class Side(value: Byte)
 
-  @newtype
-  case class ADKey(value: Array[Byte])
-
-  @newtype
-  case class ADValue(value: Array[Byte])
-
-  @newtype
-  case class ADDigest(value: Array[Byte])
-
-  @newtype
-  case class SerializedAdProof(value: Array[Byte])
-
-  @newtype
-  case class Balance(value: Byte)
-
   /** Immutable empty array which can be used in many places to avoid allocations. */
   val EmptyByteArray = Array.empty[Byte]
 
