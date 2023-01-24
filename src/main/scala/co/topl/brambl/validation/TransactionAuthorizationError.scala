2d927b3988c598f2cd3c713d1e6df880005daf33
Diadem <d.shoukralla@topl.me>
2023-01-24 16:29:41 -0500

    TSDK-240 Migration Part 1 (#7)
    * Added Protobuf and Quivr4s dependencies
    
    * Added the bare minimum traits, algebras, and errors
    
    * Added Tags and ContainsSignable to common package
    
    * Ran formatter
    
    * Added TransactionAuthorizationInterpreter

---
+++ src/main/scala/co/topl/brambl/validation/TransactionAuthorizationError.scala
@@ -0,0 +1,29 @@
+package co.topl.brambl.validation
+
+import co.topl.quivr.runtime.QuivrRuntimeError
+
+sealed abstract class TransactionAuthorizationError extends ValidationError
+
+object TransactionAuthorizationError {
+
+  /**
+   * Generic error for when a transaction is not authorized
+   *
+   * Temporary until the interpreter utilized Contextual and Permanent errors
+   */
+  case object AuthorizationFailed extends TransactionAuthorizationError
+
+  /**
+   * An Authorization error indicating that this transaction was invalid only within the provided validation context.
+   * It _might_ become valid later (or perhaps it _was_ valid previously)
+   * (i.e. height lock)
+   */
+  case class Contextual(error: QuivrRuntimeError) extends TransactionAuthorizationError
+
+  /**
+   * An Authorization error indicating that this transaction will never be valid.  This is usually the result of something
+   * that could _probably_ be determined in a syntactic validation check, meaning no context is needed to perform the
+   * authorization validation.  (i.e. invalid signature)
+   */
+  case class Permanent(error: QuivrRuntimeError) extends TransactionAuthorizationError
+}
