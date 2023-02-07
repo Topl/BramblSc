540a1bbbc69fc3b682d2dc5a5b7bd11492c1f00c
Diadem Shoukralla <d.shoukralla@topl.me>
2023-02-06 19:35:58 -0500

    Removed a TODO

--- brambl-sdk/src/main/scala/co/topl/brambl/validation/algebras/TransactionSyntaxVerifier.scala
+++ brambl-sdk/src/main/scala/co/topl/brambl/validation/algebras/TransactionSyntaxVerifier.scala
@@ -5,7 +5,5 @@ import co.topl.common.ContextlessValidation
 import co.topl.brambl.validation.TransactionSyntaxError
 import cats.data.NonEmptyChain
 
-// TODO: A decision should be made about what the return type of `validate` should be.
-//  ValidatedNec? ValidatedNel? Either[List[Errors], Tx]?
 trait TransactionSyntaxVerifier[F[_]]
     extends ContextlessValidation[F, NonEmptyChain[TransactionSyntaxError], IoTransaction]
