4d206e11ef88837d102f0d3f9da27c126641bfcc
Diadem <d.shoukralla@topl.me>
2023-02-13 14:55:24 -0500

    TSDK-279 CredentiallerInterpreter (#14)
    * First pass at refactoring Credentialler. 
    
    Notable differences 1) validation will do syntax and authorization validation. 2) Signatures updated to be simpler since at this moment in time its not clear why we would need errors during the prove stage. It was originally added because of a Syntax check that no longer upholds
    
    * Removed hardcoded usage of the MockDataApi
    
    * Cleaned up the private functions within CredentiallerInterpreter; updated documentation and also replaced Option[Proof] with Proof() (i.e, Proof.Value.Empty)
    
    * Removed hardcoded Id from helper functions. Instead use F context
    
    * Ran formatter
    
    * Moved MockDataApi to the tests directory. Also cleaned up MockHelpers
    
    MockDataApi will work in conjunction with MockHelpers. This is in preparation for CredentiallerInterpreterSpec. Ensured all existing tests pass
    
    * Implemented basic unit tests for credentialler
    
    * Ran formatter

--- brambl-sdk/src/main/scala/co/topl/brambl/wallet/Credentialler.scala
+++ brambl-sdk/src/main/scala/co/topl/brambl/wallet/Credentialler.scala
@@ -2,14 +2,46 @@ package co.topl.brambl.wallet
 
 import co.topl.brambl.Context
 import co.topl.brambl.models.transaction.IoTransaction
-import co.topl.brambl.validation.{TransactionAuthorizationError, TransactionSyntaxError, ValidationError}
+import co.topl.brambl.validation.ValidationError
+import quivr.models.Proof
 
-trait Credentialler {
-  def prove(unprovenTx: IoTransaction): Either[List[TransactionSyntaxError], IoTransaction]
-  def validate(tx:      IoTransaction, ctx: Context): List[TransactionAuthorizationError]
+/**
+ * Defines a Credentialler. A Credentialler is responsible for proving and verifying transactions.
+ */
+trait Credentialler[F[_]] {
 
+  /**
+   * Prove a transaction. That is, prove all the inputs within the transaction if possible
+   *
+   * Note: If a proposition is unable to be proven, it's proof will be [[Proof.Value.Empty]]
+   *
+   * @param unprovenTx The unproven transaction to prove
+   * @return The proven version of the transaction.
+   */
+  def prove(unprovenTx: IoTransaction): F[IoTransaction]
+
+  /**
+   * Validate whether the transaction is syntactically valid and authorized.
+   * A Transaction is authorized if all contained attestations are satisfied
+   *
+   * TODO: Revisit when we have cost estimator to decide which validations should occur
+   *
+   * @param tx  Transaction to validate
+   * @param ctx Context to validate the transaction in
+   * @return List of validation errors, if any
+   */
+  def validate(tx: IoTransaction, ctx: Context[F]): F[List[ValidationError]]
+
+  /**
+   * Prove and validate a transaction.
+   * That is, attempt to prove all the inputs within the transaction and then validate if the transaction
+   * is syntactically valid and successfully proven
+   *
+   * @param unprovenTx The unproven transaction to prove
+   * @return The proven version of the input if valid. Else the validation errors
+   */
   def proveAndValidate(
     unprovenTx: IoTransaction,
-    ctx:        Context
-  ): Either[List[ValidationError], IoTransaction]
+    ctx:        Context[F]
+  ): F[Either[List[ValidationError], IoTransaction]]
 }
--- brambl-sdk/src/test/scala/co/topl/brambl/MockHelpers.scala
+++ brambl-sdk/src/test/scala/co/topl/brambl/MockHelpers.scala
@@ -6,15 +6,36 @@ import co.topl.brambl.common.ContainsImmutable.ContainsImmutableTOps
 import co.topl.brambl.common.ContainsImmutable.instances._
 import co.topl.brambl.common.ContainsSignable.ContainsSignableTOps
 import co.topl.brambl.common.ContainsSignable.instances._
-import co.topl.brambl.dataApi.MockDataApi
 import co.topl.brambl.models._
 import co.topl.brambl.models.box.{Lock, Value}
 import co.topl.brambl.models.transaction._
+import co.topl.brambl.routines.digests.Blake2b256Digest
+import co.topl.brambl.routines.signatures.Ed25519Signature
+import co.topl.proto.models.Proof
 import co.topl.quivr.api.{Proposer, Prover}
 import com.google.protobuf.ByteString
-import quivr.models.{Int128, Proof, SignableBytes, SmallData}
+import quivr.models.{Int128, Preimage, SignableBytes, SmallData}
 
 trait MockHelpers {
+
+  val MockSecret: Array[Byte] = "A mock secret".getBytes
+  val MockPreimage: Preimage = Preimage(ByteString.copyFrom(MockSecret), ByteString.copyFromUtf8("salt"))
+
+  val txDatum: Datum.IoTransaction = Datum.IoTransaction(
+    Event
+      .IoTransaction(
+        Schedule(3, 50, 100),
+        List(),
+        List(),
+        SmallData(ByteString.copyFrom("metadata".getBytes))
+      )
+  )
+
+  // Arbitrary Transaction that any new transaction can reference
+  val dummyTx: IoTransaction = IoTransaction(datum = txDatum)
+
+  val dummyTxIdentifier: KnownIdentifier.TransactionOutput32 =
+    KnownIdentifier.TransactionOutput32(0, 0, 0, Identifier.IoTransaction32(dummyTx.sized32Evidence))
 
   val outDatum: Datum.UnspentOutput =
     Datum.UnspentOutput(Event.UnspentTransactionOutput(SmallData(ByteString.copyFrom("metadata".getBytes))))
@@ -30,46 +51,53 @@ trait MockHelpers {
 
   val address: Address =
     Address(0, 0, Identifier().withLock32(Identifier.Lock32(trivialOutLock.sized32Evidence)))
-  val knownId: KnownIdentifier = KnownIdentifier().withTransactionOutput32(MockDataApi.dummyTxIdentifier2a)
+  val knownId: KnownIdentifier = KnownIdentifier().withTransactionOutput32(dummyTxIdentifier)
 
-  val inLock: Lock.Predicate = Lock.Predicate(
+  val inLockFull: Lock.Predicate = Lock.Predicate(
     List(
       Proposer.LockedProposer[Id].propose(None),
+      Proposer
+        .digestProposer[Id]
+        .propose(
+          (
+            // Hardcoding Blake2b256Digest
+            Blake2b256Digest.routine,
+            Blake2b256Digest.hash(MockPreimage)
+          )
+        ),
+      Proposer
+        .signatureProposer[Id]
+        .propose(
+          (
+            // Hardcoding Ed25519Signature
+            Ed25519Signature.routine,
+            Ed25519Signature.createKeyPair(MockSecret).vk.get // Get will be removed once validation rules are in place
+          )
+        ),
       Proposer.heightProposer[Id].propose(("header", 0, 100)),
       Proposer.tickProposer[Id].propose((0, 100))
     ),
-    1
+    3
   )
   val fakeMsgBind: SignableBytes = "transaction binding".getBytes.immutable.signable
 
-  val txDatum: Datum.IoTransaction = Datum.IoTransaction(
-    Event
-      .IoTransaction(
-        Schedule(3, 50, 100),
-        List(),
-        List(),
-        SmallData(ByteString.copyFrom("metadata".getBytes))
+  val nonEmptyAttestation: Attestation = Attestation().withPredicate(
+    Attestation.Predicate(
+      inLockFull,
+      List(
+        Prover.lockedProver[Id].prove((), fakeMsgBind),
+        Prover.heightProver[Id].prove((), fakeMsgBind),
+        Prover.tickProver[Id].prove((), fakeMsgBind)
       )
+    )
   )
+
   val output: UnspentTransactionOutput = UnspentTransactionOutput(address, value, outDatum)
 
-  val inResponsesFull: Seq[Proof] = List(
-    Prover.lockedProver[Id].prove((), fakeMsgBind),
-    Prover.heightProver[Id].prove((), fakeMsgBind),
-    Prover.tickProver[Id].prove((), fakeMsgBind)
-  )
-  val inResponsesEmpty: Seq[Proof] = List()
-
-  val attFull: Attestation = Attestation().withPredicate(Attestation.Predicate(inLock, inResponsesFull))
+  val attFull: Attestation = Attestation().withPredicate(Attestation.Predicate(inLockFull, List()))
 
   val inputFull: SpentTransactionOutput =
     SpentTransactionOutput(knownId, attFull, value, inDatum, List())
 
-  val attEmpty: Attestation = Attestation().withPredicate(Attestation.Predicate(inLock, inResponsesEmpty))
-
-  val inputEmpty: SpentTransactionOutput =
-    SpentTransactionOutput(knownId, attEmpty, value, inDatum, List())
-
   val txFull: IoTransaction = IoTransaction(List(inputFull), List(output), txDatum)
-  val txEmpty: IoTransaction = IoTransaction(List(inputEmpty), List(output), txDatum)
 }
