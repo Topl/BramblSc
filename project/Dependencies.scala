c2f1553757e65e70af58a5fe55d5e8dd85a78acb
Scala Steward <scala_steward@virtuslab.com>
2023-02-16 23:19:13 +0000

    Update circe-core to 0.14.4 in main

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ object Dependencies {
   object Versions {
     val catsCoreVersion = "2.8.0"
     val simulacrumVersion = "1.0.1"
-    val circeVersion = "0.14.3"
-    val quivr4sVersion = "ef14fe2"  // scala-steward:off
+    val circeVersion = "0.14.4"
+    val quivr4sVersion = "ef14fe2" // scala-steward:off
     val protobufSpecsVersion = "c920f90" // scala-steward:off
     val mUnitTeVersion = "0.7.29"
   }
