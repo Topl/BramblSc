b6c9108243b1d85c0e1a2d05b0365eae41716f4a
Edmundo Lopez <e.lopez@topl.me>
2023-04-28 11:11:57 -0400

    TSDK-467 Add AddressCodecs functions

--- brambl-sdk/src/main/scala/co/topl/brambl/codecs/AddressCodecs.scala
+++ brambl-sdk/src/main/scala/co/topl/brambl/codecs/AddressCodecs.scala
@@ -1,3 +1,36 @@
 package co.topl.brambl.codecs
 
-object AddressCodecs {}
+import co.topl.brambl.utils.EncodingError
+import co.topl.brambl.models.LockAddress
+import co.topl.brambl.utils.Encoding._
+import co.topl.brambl.models.Evidence
+import quivr.models.Digest
+import com.google.protobuf.ByteString
+import co.topl.brambl.models.LockId
+
+object AddressCodecs {
+
+  def decodeAddress(
+    address: String
+  ): Either[EncodingError, LockAddress] =
+    for {
+      byteArray <- decodeFromBase58Check(address)
+      (network, ledgerAndId) = byteArray.splitAt(4)
+      (ledger, id) = ledgerAndId.splitAt(4)
+      lockAddress = LockAddress(
+        network.head,
+        ledger.head,
+        LockId((ByteString.copyFrom(id)))
+      )
+    } yield lockAddress
+
+  def encodeAddress(lockAddress: LockAddress): String = {
+    val networkByteArray = BigInt(lockAddress.network).toByteArray
+    val network = Array.fill(4 - networkByteArray.length)(0.toByte) ++ networkByteArray
+    val ledgerByteArray = BigInt(lockAddress.ledger).toByteArray
+    val ledger = Array.fill(4 - ledgerByteArray.length)(0.toByte) ++ ledgerByteArray
+    val id = lockAddress.id.value.toByteArray
+    encodeToBase58Check(network ++ ledger ++ id)
+  }
+
+}
