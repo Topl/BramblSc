04d1077d65240f08feb8b539e3ed67e76421f706
Fernando <f.torterolo@topl.me>
2023-05-17 18:21:17 -0300

    Update Dependencies.scala (#62)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,8 +7,8 @@ object Dependencies {
     val catsCoreVersion = "2.9.0"
     val simulacrumVersion = "1.0.1"
     val circeVersion = "0.14.5"
-    val quivr4sVersion = "9cfc8b4" // scala-steward:off
-    val protobufSpecsVersion = "e7b3bec" // scala-steward:off
+    val quivr4sVersion = "daa517d" // scala-steward:off
+    val protobufSpecsVersion = "8bb8a3b" // scala-steward:off
     val mUnitTeVersion = "0.7.29"
   }
 
