111f9e447a8cb1b15243ef46ee54656efbf2de05
Scala Steward <scala_steward@virtuslab.com>
2023-06-03 05:54:57 +0000

    Update scalacheck-1-14 to 3.2.11.0 in main

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
 
   val scalacheck: Seq[ModuleID] = Seq(
     "org.scalacheck"    %% "scalacheck"      % "1.17.0",
-    "org.scalatestplus" %% "scalacheck-1-14" % "3.2.2.0"
+    "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0"
   )
 
   val scalamock: Seq[ModuleID] = Seq(
