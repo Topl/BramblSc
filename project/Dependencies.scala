98bee3286f554951542f7b38fea1ac3403957c7b
Edmundo López Bóbeda <e.lopez@topl.me>
2023-07-17 15:45:08 -0400

    Update quivr4s to 2.0.0-alpha2
    Update quivr4s to 2.0.0-alpha2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val catsCoreVersion = "2.9.0"
     val simulacrumVersion = "1.0.1"
     val circeVersion = "0.14.5"
-    val quivr4sVersion = "2.0.0-alpha1"
+    val quivr4sVersion = "2.0.0-alpha2"
     val protobufSpecsVersion = "2.0.0-alpha2"
     val mUnitTeVersion = "0.7.29"
   }
