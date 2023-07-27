fc834fa99e3132126f72c151e7c542d8279a937c
Diadem <d.shoukralla@topl.me>
2023-07-27 00:40:19 -0400

    Updated tests with mnemonic persisting changes

--- brambl-sdk/src/test/scala/co/topl/brambl/wallet/WalletApiSpec.scala
+++ brambl-sdk/src/test/scala/co/topl/brambl/wallet/WalletApiSpec.scala
@@ -310,4 +310,11 @@ class WalletApiSpec extends munit.FunSuite with MockHelpers {
     assert(importedWallet.isLeft)
     assert(importedWallet.left.toOption.get == WalletApi.FailedToSaveWallet(MockWalletKeyApi.MainKeyVaultSaveFailure))
   }
+
+  test("saveMnemonic: verify a simple save") {
+    val name = "test"
+    val res = walletApi.saveMnemonic(IndexedSeq("a", "b", "c"), name)
+    assert(res.isRight)
+    assert(MockWalletKeyApi.mnemonicInstance.contains(name))
+  }
 }
