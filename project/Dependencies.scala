ff5c43b1754beb51ccd31113b695f3a5e203298a
Scala Steward <scala_steward@virtuslab.com>
2023-08-01 20:15:02 +0000

    Update sqlite-jdbc to 3.42.0.0 in main

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -65,7 +65,7 @@ object Dependencies {
   )
 
   val sqlite: Seq[ModuleID] = Seq(
-    "org.xerial" % "sqlite-jdbc" % "3.41.2.2"
+    "org.xerial" % "sqlite-jdbc" % "3.42.0.0"
   )
 
   object Crypto {
