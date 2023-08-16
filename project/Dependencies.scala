58f4af0f323eeba5e3383cd34a12d7826012d726
Edmundo López Bóbeda <e.lopez@topl.me>
2023-08-16 14:57:16 -0400

    Update dependencies for the latest protobuf specs (#95)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val catsCoreVersion = "2.9.0"
     val simulacrumVersion = "1.0.1"
     val circeVersion = "0.14.5"
-    val protobufSpecsVersion = "2.0.0-alpha2+10-39b5e004-SNAPSHOT"
+    val protobufSpecsVersion = "2.0.0-alpha3"
     val mUnitTeVersion = "0.7.29"
   }
 
