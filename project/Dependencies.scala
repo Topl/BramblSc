339dbcd5967074c63afe7f662accd6b71c663fd3
Scala Steward <scala_steward@virtuslab.com>
2023-09-11 00:07:28 +0000

    Update scalatest to 3.2.17 in main

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   )
 
   val scalatest: Seq[ModuleID] = Seq(
-    "org.scalatest"    %% "scalatest"                     % "3.2.16",
+    "org.scalatest"    %% "scalatest"                     % "3.2.17",
     "com.ironcorelabs" %% "cats-scalatest"                % "3.1.1",
     "org.typelevel"    %% "cats-effect-testing-scalatest" % "1.5.0"
   )
