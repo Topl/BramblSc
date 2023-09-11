0682f2fff7df92f32e4c96e7fd02a9b72d5b51b8
Scala Steward <scala_steward@virtuslab.com>
2023-09-11 00:07:35 +0000

    Update cats-core to 2.10.0 in main

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val catsCoreVersion = "2.9.0"
+    val catsCoreVersion = "2.10.0"
     val simulacrumVersion = "1.0.1"
     val circeVersion = "0.14.6"
     val protobufSpecsVersion = "2.0.0-alpha4+1-d751a916-SNAPSHOT"
