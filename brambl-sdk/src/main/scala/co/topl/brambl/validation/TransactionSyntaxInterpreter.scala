0b83df0ecbf88747f6520fca08328de087346683
Fernando <f.torterolo@topl.me>
2023-09-19 17:29:39 -0300

    Bn 1201 minting validation (#111)
    * wip, is minting inputs, outputs, case1,case2, next step case 3
    
    * minting validations, case A, case B
    
    * review comments
    
    * Update brambl-sdk/src/test/scala/co/topl/brambl/validation/TransactionSyntaxInterpreterMintingCaseBSpec.scala
    
    Co-authored-by: Diadem <d.shoukralla@topl.me>
    
    ---------
    
    Co-authored-by: Diadem <d.shoukralla@topl.me>

--- brambl-sdk/src/main/scala/co/topl/brambl/validation/TransactionSyntaxInterpreter.scala
+++ brambl-sdk/src/main/scala/co/topl/brambl/validation/TransactionSyntaxInterpreter.scala
@@ -4,10 +4,11 @@ import cats.Applicative
 import cats.implicits._
 import cats.data.{Chain, NonEmptyChain, Validated, ValidatedNec}
 import co.topl.brambl.models.box.{Lock, Value}
-import co.topl.brambl.models.transaction.IoTransaction
+import co.topl.brambl.models.transaction.{IoTransaction, SpentTransactionOutput, UnspentTransactionOutput}
 import co.topl.brambl.validation.algebras.TransactionSyntaxVerifier
 import co.topl.brambl.common.ContainsImmutable.ContainsImmutableTOps
 import co.topl.brambl.common.ContainsImmutable.instances._
+import co.topl.brambl.models.TransactionOutputAddress
 import co.topl.brambl.models.box.Attestation
 import co.topl.brambl.syntax._
 import quivr.models.{Int128, Proof, Proposition}
@@ -39,7 +40,8 @@ object TransactionSyntaxInterpreter {
       attestationValidation,
       dataLengthValidation,
       assetEqualFundsValidation,
-      assetNoRepeatedUtxosValidation
+      assetNoRepeatedUtxosValidation,
+      mintingValidation
     )
 
   /**
@@ -323,4 +325,85 @@ object TransactionSyntaxInterpreter {
       )
       .fold(().validNec[TransactionSyntaxError])(_.invalid[Unit])
 
+  private def mintingInputsProjection(transaction: IoTransaction): Seq[SpentTransactionOutput] =
+    transaction.inputs.filter { stxo =>
+      !stxo.value.value.isTopl &&
+      !stxo.value.value.isAsset &&
+      (!stxo.value.value.isLvl || (transaction.groupPolicies.exists(
+        _.event.registrationUtxo == stxo.address
+      ) || transaction.seriesPolicies.exists(_.event.registrationUtxo == stxo.address)))
+    }
+
+  private def mintingOutputsProjection(transaction: IoTransaction): Seq[UnspentTransactionOutput] = {
+    val groupIdsOnMintedStatements =
+      transaction.inputs
+        .filter(_.value.value.isGroup)
+        .filter(sto => transaction.mintingStatements.map(_.groupTokenUtxo).contains(sto.address))
+        .map(_.value.getGroup.groupId)
+
+    val seriesIdsOnMintedStatements =
+      transaction.inputs
+        .filter(_.value.value.isSeries)
+        .filter(sto => transaction.mintingStatements.map(_.seriesTokenUtxo).contains(sto.address))
+        .map(_.value.getSeries.seriesId)
+
+    transaction.outputs.filter { utxo =>
+      !utxo.value.value.isLvl &&
+      !utxo.value.value.isTopl &&
+      (!utxo.value.value.isGroup || transaction.groupPolicies
+        .map(_.event.computeId)
+        .contains(utxo.value.getGroup.groupId)) &&
+      (!utxo.value.value.isSeries || transaction.seriesPolicies
+        .map(_.event.computeId)
+        .contains(utxo.value.getSeries.seriesId)) &&
+      (!utxo.value.value.isAsset || (utxo.value.getAsset.groupId.exists(
+        groupIdsOnMintedStatements.contains
+      ) && utxo.value.getAsset.seriesId.exists(seriesIdsOnMintedStatements.contains)))
+    }
+  }
+
+  private def mintingValidation(transaction: IoTransaction) = {
+    val projectedTransaction = transaction
+      .withInputs(mintingInputsProjection(transaction))
+      .withOutputs(mintingOutputsProjection(transaction))
+
+    val groups = projectedTransaction.outputs.filter(_.value.value.isGroup).map(_.value.getGroup)
+    val series = projectedTransaction.outputs.filter(_.value.value.isSeries).map(_.value.getSeries)
+    val assets = projectedTransaction.outputs.filter(_.value.value.isAsset).map(_.value.getAsset)
+
+    def registrationInPolicyContainsLvls(registrationUtxo: TransactionOutputAddress): Boolean =
+      projectedTransaction.inputs.exists { stxo =>
+        stxo.value.value.isLvl &&
+        stxo.value.getLvl.quantity > 0 &&
+        stxo.address == registrationUtxo
+      }
+
+    val validGroups = groups.forall { group =>
+      transaction.groupPolicies.exists { policy =>
+        policy.event.computeId == group.groupId &&
+        registrationInPolicyContainsLvls(policy.event.registrationUtxo)
+      } &&
+      group.quantity > 0
+    }
+
+    val validSeries = series.forall { series =>
+      transaction.seriesPolicies.exists { policy =>
+        policy.event.computeId == series.seriesId &&
+        registrationInPolicyContainsLvls(policy.event.registrationUtxo) &&
+        series.quantity > 0
+      }
+    }
+
+    val validAssets: Boolean = true // TODO
+
+    Validated.condNec(
+      validGroups && validSeries && validAssets,
+      (),
+      TransactionSyntaxError.InsufficientInputFunds(
+        transaction.inputs.map(_.value.value).toList,
+        transaction.outputs.map(_.value.value).toList
+      )
+    )
+
+  }
 }
