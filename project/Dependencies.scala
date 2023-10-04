fb3f4fb61859422606046d98a0e9af685a442294
Scala Steward <scala_steward@virtuslab.com>
2023-10-04 04:04:14 +0000

    Update cats-effect to 3.5.2 in main

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -50,7 +50,7 @@ object Dependencies {
     "org.typelevel" %% "cats-core"   % catsCoreVersion,
     "org.typelevel" %% "mouse"       % "1.2.1",
     "org.typelevel" %% "cats-free"   % catsCoreVersion,
-    "org.typelevel" %% "cats-effect" % "3.5.1"
+    "org.typelevel" %% "cats-effect" % "3.5.2"
   )
 
   val simulacrum: Seq[ModuleID] = Seq(
