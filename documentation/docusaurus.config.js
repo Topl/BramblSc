cd9932615bca279bf22dfece9a6c0ef405b04d3a
Diadem <d.shoukralla@topl.me>
2023-10-09 20:40:22 -0400

    TSDK-603 Setup Documentation Portal (#130)
    * Initial commit. Added base doc site with Topl branding
    
    * Created the structure of what the pages may look be
    
    * Added gh pages workflow
    
    * Added pseudo versioning
    
    * removed excess files from tutorials
    
    * removed docusaurus images
    
    * removed unrelated readme

---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,128 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'BramblSc',
+  tagline: 'Build using Topl\'s Scala Software Development Kit',
+  url: 'https://topl.github.io/',
+  baseUrl: '/BramblSc',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Topl', // Usually your GitHub org/user name.
+  projectName: 'BramblSc', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          lastVersion: 'current',
+          versions: {
+            current: {
+              label: '2.0.0',
+              path: '2.0.0',
+              badge: true,
+            },
+          },
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        style: 'dark',
+        title: '',
+        logo: {
+          alt: 'Topl Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'referenceSidebar',
+            position: 'left',
+            label: 'Reference',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialsSidebar',
+            position: 'left',
+            label: 'Tutorials',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'conceptsSidebar',
+            position: 'left',
+            label: 'Concepts',
+          },
+          {
+            href: 'https://github.com/Topl/BramblSc',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Blog',
+                href: 'https://medium.com/topl-blog',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/topl',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/topl_protocol',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Contribute',
+                href: 'https://github.com/Topl/BramblSc',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Topl, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
