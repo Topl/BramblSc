aaf3adbaeba68cb17bf8fc484c5e524f7fd11af4
Diadem <d.shoukralla@topl.me>
2023-10-11 17:24:23 -0400

    Added test for race condition

---
+++ brambl-sdk/src/test/scala/co/topl/brambl/wallet/ExtendedEd25519ResourceSpec.scala
@@ -0,0 +1,32 @@
+package co.topl.brambl.wallet
+
+import cats.arrow.FunctionK
+import cats.implicits.{catsSyntaxTuple7Parallel, catsSyntaxTuple9Parallel}
+import co.topl.brambl.models.Indices
+import co.topl.brambl.{MockHelpers, MockWalletKeyApi}
+
+class ExtendedEd25519ResourceSpec extends munit.CatsEffectSuite with MockHelpers {
+  implicit val idToId: FunctionK[F, F] = FunctionK.id[F]
+
+  test("Race condition") {
+    val NumRuns = 1000000
+    // a single WalletApi instance to call diff methods in parallel
+    val walletApi = WalletApi.make[F](MockWalletKeyApi)
+
+    // testing multiple functions in parallel.
+    def runParallel(idx: Int): F[Unit] = (
+      walletApi.deriveChildKeys(MockMainKeyPair, Indices(0, 0, 0)),
+      walletApi.deriveChildKeys(MockMainKeyPair, Indices(idx, idx, idx)),
+      walletApi.deriveChildKeysPartial(MockMainKeyPair, 0, 0),
+      walletApi.deriveChildKeysPartial(MockMainKeyPair, idx, idx),
+      walletApi.deriveChildVerificationKey(MockMainKeyPair.vk, 0),
+      walletApi.deriveChildVerificationKey(MockMainKeyPair.vk, idx),
+      walletApi.createNewWallet("password".getBytes),
+    ).parMapN((_, _, _, _, _, _, _) => ()) // the results do no matter. We are testing if an exception occurs
+
+    0 to NumRuns foreach { idx =>
+      assertIO_(runParallel(idx))
+    }
+  }
+
+}
