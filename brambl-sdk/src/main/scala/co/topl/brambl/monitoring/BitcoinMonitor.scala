0c3ea555aa192a92d289bd7c271007c8d2f9a585
Diadem <d.shoukralla@topl.me>
2024-03-12 08:14:31 -0400

    Added scaladoc

--- brambl-sdk/src/main/scala/co/topl/brambl/monitoring/BitcoinMonitor.scala
+++ brambl-sdk/src/main/scala/co/topl/brambl/monitoring/BitcoinMonitor.scala
@@ -20,10 +20,21 @@ import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
 
 
+/**
+ * Class to monitor incoming bitcoin blocks via a queue.
+ * @param blockQueue The queue in which new blocks will be added to
+ * @param startingBlocks Past blocks that should be reported.
+ * @param zmqHost The host where the monitor will be subscribed to
+ * @param zmqPort The port where the monitor will be subscribed to
+ */
 class BitcoinMonitor(blockQueue: Queue[IO, BitcoinBlock], startingBlocks: Vector[BitcoinBlock], zmqHost: String, zmqPort: Int) {
   val subscriber: ZMQSubscriber = initZmqSubscriber(blockQueue, zmqHost, zmqPort)
   subscriber.start()
 
+  /**
+   * Return a stream of blocks.
+   * @return The infinite stream of blocks. If startingBlocks was provided, they will be at the front of the stream.
+   */
   def monitorBlocks(): Stream[IO, BitcoinBlock] =
     Stream.emits(startingBlocks) ++ Stream.fromQueueUnterminated(blockQueue)
 }
@@ -32,7 +43,15 @@ object BitcoinMonitor {
   object Bitcoind {
     implicit val system: ActorSystem = ActorSystem("System")
 
-    // Connection to the bitcoind RPC server instance
+    /**
+     * Connection to the bitcoind RPC server instance
+     * @param network Parameters of a given network to be used
+     * @param host The host to connect to the bitcoind instance
+     * @param rpcUser rpc username
+     * @param rpcPassword rpc password
+     * @param binary the bitcoind executable
+     * @return
+     */
     def connection(network: NetworkParameters, host: String, rpcUser: String, rpcPassword: String, binary: File): BitcoindInstanceLocal = BitcoindInstanceLocal(
       network = network,
       uri = new URI(s"$host:${network.port}"),
@@ -41,6 +60,11 @@ object BitcoinMonitor {
       binary = binary
     )
   }
+
+  /**
+   * A wrapper for a bitcoin Block.
+   * @param block The bitcoin Block being wrapped
+   */
   case class BitcoinBlock(block: Block){
     def transactions[F[_]]: Stream[F, Transaction] = Stream.emits(block.transactions)
   }
@@ -51,9 +75,24 @@ object BitcoinMonitor {
     blockQueue.offer(BitcoinBlock(block)).unsafeRunSync()
   }
 
+  /**
+   * Initialize a ZeroMQ subscriber that adds new blocks to a queue
+   * @param blockQueue The queue to add new blocks to
+   * @param host The host in which the ZmqSubscriber will be connected to
+   * @param port The port in which the ZmqSubscriber will be connected to
+   * @return An initialized ZMQSubcriber instance
+   */
   def initZmqSubscriber(blockQueue: Queue[IO, BitcoinBlock], host: String, port: Int): ZMQSubscriber =
     new ZMQSubscriber(new InetSocketAddress(host, port), None, None, None, Some(addToQueue(blockQueue)))
 
+  /**
+   * Initialize and return a BitcoinMonitor instance.
+   * @param bitcoindInstance The local bitcoind instance to monitor
+   * @param startBlock The hash of a past block. Used to retroactively report blocks. The bitcoin monitor will report all blocks starting at this block.
+   * @param zmqHost The host in which the underlying ZmqSubscriber will be connected to. This is used to capture newly minted blocks.
+   * @param zmqPort The port in which the underlying ZmqSubscriber will be connected to. This is used to capture newly minted blocks.
+   * @return An instance of a BitcoinMonitor
+   */
   def apply(bitcoindInstance: BitcoindInstanceLocal, startBlock: Option[DoubleSha256DigestBE] = None, zmqHost: String = "127.0.0.1", zmqPort: Int = 28332): IO[BitcoinMonitor] = {
     import cats.effect.unsafe.implicits.global
     implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
@@ -68,7 +107,7 @@ object BitcoinMonitor {
       case None => prevHashes
     }
     val existingHashes = getBlockHashes(startBlock)
-    println("Fetching blocks:")
+    println("Retroactively fetching blocks:")
     existingHashes.foreach(h => println(h.hex))
     for {
       blockQueue <- Queue.unbounded[IO, BitcoinBlock]
