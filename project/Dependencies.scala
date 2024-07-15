e9da5462eca2b1c2c2cef8525cd399d00e478826
Edmundo Lopez <e.lopez@topl.me>
2024-07-15 12:19:38 -0400

    Update dependency bitcoin s

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val circeVersion = "0.14.6"
     val protobufSpecsVersion = "2.0.0-beta3"
     val mUnitTeVersion = "0.7.29"
-    val btcVersion = "1.9.7"
+    val btcVersion = "1.9.9"
     val btcVersionZmq = "1.9.8"
   }
 
