39bb1e36f2d80047246f0df85049a1ebde76d6e3
Diadem Shoukralla <d.shoukralla@topl.me>
2024-08-14 12:23:09 -0400

    Ran formatter

--- brambl-sdk/src/main/scala/co/topl/brambl/display/TypeIdentifierDisplayOps.scala
+++ brambl-sdk/src/main/scala/co/topl/brambl/display/TypeIdentifierDisplayOps.scala
@@ -8,11 +8,12 @@ import co.topl.brambl.utils.Encoding
 trait TypeIdentifierDisplayOps {
 
   implicit val typeIdentifierDisplay: DisplayOps[ValueTypeIdentifier] = {
-    case LvlType => "LVL"
-    case ToplType(_) => "TOPL"
-    case GroupType(groupId) => s"Group(${groupId.display})"
+    case LvlType              => "LVL"
+    case ToplType(_)          => "TOPL"
+    case GroupType(groupId)   => s"Group(${groupId.display})"
     case SeriesType(seriesId) => s"Series(${seriesId.display})"
-    case AssetType(groupIdOrAlloy, seriesIdOrAlloy) => s"Asset(${Encoding.encodeToHex(groupIdOrAlloy.toByteArray)}, ${Encoding.encodeToHex(seriesIdOrAlloy.toByteArray)})"
+    case AssetType(groupIdOrAlloy, seriesIdOrAlloy) =>
+      s"Asset(${Encoding.encodeToHex(groupIdOrAlloy.toByteArray)}, ${Encoding.encodeToHex(seriesIdOrAlloy.toByteArray)})"
     case _ => "Unknown txo type"
   }
 }
